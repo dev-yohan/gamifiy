27a21138f4e7bb098932947e6e9fb5fd21db8585
kalelc <andreskal@gmail.com>
2014-11-10 21:21:32 -0300

    add spanish languaje to rails admin adn create bagle model

---
+++ app/models/bagle.rb
@@ -0,0 +1,5 @@
+class Bagle
+  include Mongoid::Document
+  field :name, type: String
+  field :image, type: String
+end
---
+++ test/models/bagle_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BagleTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
