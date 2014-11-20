5fcd312b887f4aa2d9661acf13b5b31867e714d7
dev-yohan <yohanmorales@hotmail.com>
2014-11-20 11:25:57 -0500

    tokenizing apps for api access

--- app/models/sites/site.rb
+++ app/models/sites/site.rb
@@ -2,6 +2,10 @@ class Sites::Site
   include Mongoid::Document
   include Mongoid::Timestamps
   include Mongoid::Slug
+
+  before_create :generate_access_token
+
+  field :access_token, type: String
   
   field :name, type: String
   slug :name
@@ -13,6 +17,15 @@ class Sites::Site
 
   belongs_to :user, :class_name => "User"
   has_many :activities, :class_name => "Activity", :dependent => "destroy"
+
+  private
   
+    def generate_access_token
+      self.access_token = loop do
+        random_token = SecureRandom.urlsafe_base64(nil, false)
+        break random_token unless self.class.where(access_token: random_token).nil?
+      end
+   end
+
 
 end
