f700c3520e3b034037070cbd8abd62716d2fe99b
dev-yohan <yohanmorales@hotmail.com>
2014-11-26 17:16:21 -0500

    quick count for activities, badges and subjects

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,8 @@ class HomeController < ApplicationController
     sites = Sites::Site.where(:user => current_user)
     @user_sites = sites.count
     @user_activities = Activity.where(:site_id.in => sites.only(:_id).map(&:_id)).count
+    @user_badges = Badge.where(:site_id.in => sites.only(:_id).map(&:_id)).count
+    @user_subjects = Subject.where(:site_id.in => sites.only(:_id).map(&:_id)).count
 
   end 
  
