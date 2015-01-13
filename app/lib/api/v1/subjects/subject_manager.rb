4a689284ea9c4606170ee0b8e3dac45397e7aafe
dev-yohan <yohanmorales@hotmail.com>
2015-01-13 15:06:56 -0500

    fixing font path

--- app/lib/api/v1/subjects/subject_manager.rb
+++ app/lib/api/v1/subjects/subject_manager.rb
@@ -1,26 +1,26 @@
-class Api::V1::Users::UserManager
+class Api::V1::Subjects::SubjectManager
 
-  #show user by id
-  def show_user(user_id)
+  #show subject by id
+  def show_subject(subject_id)
     begin
-      badge = ::Badge.find(user_id)
+      subject = ::Subject.find(subject_id)
 
-      if !badge.site.nil?
-        site = {id: badge.site.id, name: badge.site.name}
+      if !subject.site.nil?
+        site = {id: subject.site.id, name: subject.site.name}
       else
         site = {}
       end
 
-      json_data = {id: badge._id,
-        name: badge.name,
-        slug: badge.slugs.first,
-        image: badge.image.url,
+      json_data = {id: subject._id,
+        external_id: subject.external_id,
+        external_email: subject.external_email,
+        is_active: subject.is_active,
         site: site}
 
       rescue Mongoid::Errors::DocumentNotFound
         json_data = {error_code: 501,
-          dev_message: I18n.t("subjects.api.error.code_501.dev_message", id: user_id),
-          friendly_message: I18n.t("subjects.api.error.code_501.friendly_message", id: user_id)}
+          dev_message: I18n.t("subjects.api.error.code_501.dev_message", id: subject_id),
+          friendly_message: I18n.t("subjects.api.error.code_501.friendly_message", id: subject_id)}
           status = 404
         end
 
